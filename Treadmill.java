bc48cba76f91141bf58bc6559b40848540a23de4
Конпаку <sputnik-999-mel@mail.ru>
2024-02-05 09:00:22 +0500

    дорожка

---
+++ Treadmill.java
@@ -0,0 +1,27 @@
+/**
+ * Класс беговой дорожки, наследуется от базового класса.
+ * Направление - горизонтальное.
+ * Измерение - длина.
+ * Ед.изм. - метры.
+ */
+public class Treadmill extends Obstacles{
+    private static int treadMillIndex;
+    final private static int MAX_DISTANCE = 500;
+    final private static int MIN_DISTANCE = 10;
+    static {
+        treadMillIndex = 0;
+    }
+
+    public Treadmill(String name, int dimension, Directions direction) {
+        super(name, dimension, direction);
+    }
+
+    public Treadmill() {
+        this(setDefaultName("Дорожка",++treadMillIndex), setRandomDimension(MAX_DISTANCE,MIN_DISTANCE), Directions.horizontal);
+    }
+
+    @Override
+    String getUnit() {
+        return "метров";
+    }
+}
