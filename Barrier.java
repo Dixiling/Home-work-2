48c932e92bd453160c398fc0dda4837d5e9a92ab
Конпаку <sputnik-999-mel@mail.ru>
2024-02-05 08:43:23 +0500

    barrier

---
+++ Barrier.java
@@ -0,0 +1,29 @@
+/**
+ * Класс Барьер
+ * Направление - вертикально
+ * Измерение - высота
+ * Ед.изм. - сантиметры.
+ */
+public class Barrier extends Obstacles{
+    final private static int MAX_HEIGHT = 300;
+    final private static int MIN_HEIGHT = 10;
+
+    private static int barrierIndex;
+
+    static{
+        barrierIndex=0;
+    }
+    public Barrier(String name, int dimension, Directions direction) {
+        super(name, dimension, direction);
+    }
+
+    public Barrier(){
+
+        this(setDefaultName("Барьер",++barrierIndex), setRandomDimension(MAX_HEIGHT,MIN_HEIGHT),Directions.vertical);
+    }
+
+    @Override
+    String getUnit() {
+        return "см";
+    }
+}
