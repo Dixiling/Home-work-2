0d00798d69c6661dfca643dcd98844caad4ba3fd
Конпаку <sputnik-999-mel@mail.ru>
2024-02-05 08:42:26 +0500

    human

---
+++ Human.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+/**
+ * Класс Человек, наследуется от базового класса Созданий
+ */
+public class Human extends Creature {
+
+    final private static int MAX_SPEED = 5;
+    final private static int MIN_SPEED = 1;
+    final private static int MAX_DISTANCE = 1_000;
+    final private static int MIN_DISTANCE = 100;
+    final private static int MAX_HEIGHT = 200;
+    final private static int MIN_HEIGHT = 100;
+    private static int humanCount;
+    static {
+        humanCount = 0;
+    }
+
+    public Human(String name, int speed) {
+        super(name, speed);
+        Human.rand = new Random();
+        this.maxRunDistance = setMaxValue(MAX_DISTANCE,MIN_DISTANCE);
+        this.maxJumpHeight = setMaxValue(MAX_HEIGHT,MIN_HEIGHT);
+
+    }
+    public Human(){
+        this(setDefaultName("Человек", ++humanCount), setMaxValue(MAX_SPEED,MIN_SPEED));
+    }
+
+
+}
